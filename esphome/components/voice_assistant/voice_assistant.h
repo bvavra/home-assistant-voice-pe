00379c14905669dac8592004b5d003d49e54989d
Brendan Vavra <brendan.vavra@gmail.com>
2025-07-10 09:43:56 -0400

    Revert "Merge branch 'pre-merge' into dev"
    This reverts commit 49ad24dcdf566e8cd38f3d377540b8931fcdb621, reversing
    changes made to 7a098ade6ce0e2b4d02909dd2d90c74f2f04c7ba.

--- esphome/components/voice_assistant/voice_assistant.h
+++ esphome/components/voice_assistant/voice_assistant.h
@@ -44,7 +44,6 @@ enum VoiceAssistantFeature : uint32_t {
   FEATURE_API_AUDIO = 1 << 2,
   FEATURE_TIMERS = 1 << 3,
   FEATURE_ANNOUNCE = 1 << 4,
-  FEATURE_START_CONVERSATION = 1 << 5,
 };
 
 enum class State {
@@ -146,7 +145,6 @@ class VoiceAssistant : public Component {
 #ifdef USE_MEDIA_PLAYER
     if (this->media_player_ != nullptr) {
       flags |= VoiceAssistantFeature::FEATURE_ANNOUNCE;
-      flags |= VoiceAssistantFeature::FEATURE_START_CONVERSATION;
     }
 #endif
 
@@ -272,8 +270,6 @@ class VoiceAssistant : public Component {
 #endif
 #ifdef USE_MEDIA_PLAYER
   media_player::MediaPlayer *media_player_{nullptr};
-  bool media_player_wait_for_announcement_start_{false};
-  bool media_player_wait_for_announcement_end_{false};
 #endif
 
   bool local_output_{false};
